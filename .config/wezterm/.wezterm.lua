4249298fcc1d49d90bc852d4fd2b2463afef5c77
William Faunce <fonz.dev@pm.me>
2025-04-06 15:01:17 -0400

    feat: add initial WezTerm config

---
+++ .config/wezterm/.wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require('wezterm')
+local config = wezterm.config_builder()
+
+-- Font settings
+config.font_size = 14
+config.line_height = 1.2
+config.font = wezterm.font_with_fallback {
+  'Fira Code',
+  'JetBrains Mono'
+}
+
+-- Colors
+config.colors = {
+  cursor_bg = "white",
+  cursor_border = "purple",
+}
+
+-- Appearance
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.color_scheme = 'Dracula'
+
+-- Miscellaneous settings
+config.max_fps = 120
+
+return config
