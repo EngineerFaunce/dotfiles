b237e1d4911eee691e1da4dec6c83bbd1dcad544
Will Faunce <fonz.dev@pm.me>
2025-04-08 21:08:21 -0400

    chore: disable window padding config for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,12 +18,12 @@ config.colors = {
 -- Appearance
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
+-- config.window_padding = {
+--   left = 0,
+--   right = 0,
+--   top = 0,
+--   bottom = 0,
+-- }
 config.color_scheme = 'Dracula'
 config.window_background_opacity = 0.8
 
