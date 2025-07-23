289539ce27b04dc3f7d353a3d266bc3e76dc1e22
William Faunce <fonz.dev@pm.me>
2025-07-22 22:20:00 -0400

    temp: feat: add str0m workspace

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require('wezterm')
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 -- Font settings
@@ -36,5 +37,29 @@ config.keys = {
     action = wezterm.action.CloseCurrentPane { confirm = true },
   },
 }
+
+wezterm.on("gui-startup", function(cmd)
+  -- allow `wezterm start -- something` to affect what we spawn in our initial window
+  local args = {}
+  if cmd then
+    args = cmd.args
+  end
+
+  -- str0m workspace
+  local project_dir = wezterm.home_dir .. "/repos/str0m-intro"
+  local tab, terminal_pane, window = wezterm.mux.spawn_window {
+    workspace = "str0m",
+    cwd = project_dir,
+    args = args,
+  }
+  window:gui_window():maximize()
+  local editor_pane = terminal_pane:split {
+    direction = "Top",
+    size = 0.9,
+    cwd = project_dir,
+  }
+
+  mux.set_active_workspace "str0m"
+end)
 
 return config
